f1ff884a2a8ea5dc0c26ff5f1f5580c9eaa13be6
Nemya <yiduolnemya@gmail.com>
2025-01-10 15:40:14 -0500

    feat: add MainContent html content

---
+++ src/components/MainContent.tsx
@@ -0,0 +1,17 @@
+export default function MainContent() {
+  return (
+    <div>
+      <label htmlFor="">
+        <textarea name="" id=""></textarea>
+      </label>
+      <div>
+        <h2></h2>
+        <div>
+          <button>Raw</button>
+          <button>Translated</button>
+        </div>
+        <p></p>
+      </div>
+    </div>
+  );
+}
