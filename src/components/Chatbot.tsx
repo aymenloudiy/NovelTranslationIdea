a75e72e85ba333da2e37096055133c0d3e1a6128
Nemya <yiduolnemya@gmail.com>
2025-01-14 23:13:26 -0500

    feat: handle user input

--- src/components/Chatbot.tsx
+++ src/components/Chatbot.tsx
@@ -1,23 +1,28 @@
+import axios from "axios";
 import { useState } from "react";
+
 export default function Chatbot() {
-  const [response, setResponse] = useState<string>(
-    "Hi! What would you like me to translate?"
-  );
+  const [response, setResponse] = useState<string>("Hi! How can I assist you?");
   const [value, setValue] = useState<string>("");
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) =>
     setValue(e.target.value);
+  const handleSubmit = async () => {
+    const response = await axios.post("http://localhost:3005/chatbot", {
+      question: value,
+    });
+    setResponse(response.data);
+  };
   return (
     <div>
       <div className="container">
         <div>
-          <input type="text"></input>
           <input type="text" value={value} onChange={onChange}></input>
         </div>
         <div>
           <button>Send</button>
+          <button onClick={handleSubmit}>Send</button>
         </div>
         <div>
-          <p>Chatbot:</p>
           <p>Chatbot:{response}</p>
         </div>
       </div>
