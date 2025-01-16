6dd3cf3db2c148a85e522f5b50327c45c749b58a
Nemya <yiduolnemya@gmail.com>
2025-01-15 21:36:06 -0500

    fix: change input element to textarea

--- src/components/Chatbot.tsx
+++ src/components/Chatbot.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 export default function Chatbot() {
   const [response, setResponse] = useState<string>("Hi! How can I assist you?");
   const [value, setValue] = useState<string>("");
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+  const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) =>
     setValue(e.target.value);
   const handleSubmit = async () => {
     const response = await axios.post("http://localhost:8081/chatbot", {
@@ -16,7 +16,7 @@ export default function Chatbot() {
     <div>
       <div className="container">
         <div>
-          <input type="text" value={value} onChange={onChange}></input>
+          <textarea value={value} onChange={onChange}></textarea>
         </div>
         <div>
           <button>Send</button>
