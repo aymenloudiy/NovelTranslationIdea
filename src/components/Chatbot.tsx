8ab291268ed23f378373ef2feaa212a026ac4c47
Nemya <yiduolnemya@gmail.com>
2025-01-17 15:23:03 -0500

    style: add button and textarea styles

--- src/components/Chatbot.tsx
+++ src/components/Chatbot.tsx
@@ -16,11 +16,29 @@ export default function Chatbot() {
     <div>
       <div className="container">
         <p>Chatbot:{response}</p>
+
+        <label
+          htmlFor="message"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Your message
+        </label>
+        <textarea
+          value={value}
+          onChange={onChange}
+          id="message"
+          rows={4}
+          className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Write your thoughts here..."
+        ></textarea>
+
         <div>
-          <textarea value={value} onChange={onChange}></textarea>
-        </div>
-        <div>
-          <button onClick={handleSubmit}>Send</button>
+          <button
+            className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+            onClick={handleSubmit}
+          >
+            Send
+          </button>
         </div>
       </div>
     </div>
--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -3,12 +3,11 @@ export default function MainContent() {
   return (
     <main className="col-span-9">
       <Chatbot></Chatbot>
-      <div>
-        <div>
+
+      {/* <div>
           <button>Raw</button>
           <button>Translated</button>
-        </div>
-      </div>
+        </div> */}
     </main>
   );
 }
