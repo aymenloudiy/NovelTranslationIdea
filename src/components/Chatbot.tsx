04c4f06a36e5ec18a8b267b615d87a6be0de703a
Nemya <yiduolnemya@gmail.com>
2025-01-19 20:30:09 -0500

    style: move text to bottom left

--- src/components/Chatbot.tsx
+++ src/components/Chatbot.tsx
@@ -15,9 +15,9 @@ export default function Chatbot() {
   return (
     <div className="w-full">
       <div className="container h-full grid grid-rows-12">
-        <p className="mb-3 text-gray-100 dark:text-gray-100 col-span-full row-span-8">
-          {response}
-        </p>
+        <div className="flex items-end pb-8 p-8 mb-3 text-gray-100 dark:text-gray-100 col-span-full row-span-8 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500">
+          <p>{response}</p>
+        </div>
 
         {/* <label
           htmlFor="message"
@@ -30,8 +30,7 @@ export default function Chatbot() {
           value={value}
           onChange={onChange}
           id="message"
-          rows={6}
-          className=" row-span-2block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          className=" row-span-2 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           placeholder="Write your thoughts here..."
         ></textarea>
 
