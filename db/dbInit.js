38fbc0e432a3eb2693e5ca8ad169c378be5bf319
Nemya <yiduolnemya@gmail.com>
2025-01-21 21:56:18 -0500

    feat: create simple library table

--- db/dbInit.js
+++ db/dbInit.js
@@ -2,17 +2,15 @@ const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database(":memory:");
 
 db.serialize(() => {
-  db.run("CREATE TABLE lorem (info TEXT)");
-
-  const stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-  for (let i = 0; i < 10; i++) {
-    stmt.run("Ipsum " + i);
-  }
+  db.run("CREATE TABLE library (id INTEGER PRIMARY KEY, title TEXT)");
+  const mockData = [
+    [0, "A Sorcerer's Journey"],
+    [1, "Reverend Insanity"],
+  ];
+  const stmt = db.prepare("INSERT INTO library (id, title) VALUES (?,?)");
+  mockData.forEach((row) => stmt.run(row));
   stmt.finalize();
-
-  db.each("SELECT rowid AS id, info FROM lorem", (err, row) => {
-    console.log(row.id + ": " + row.info);
-  });
 });
-
 db.close();
+
+module.exports = db;
