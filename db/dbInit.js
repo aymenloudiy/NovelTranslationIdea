9d15f5e59b20fc5c36140108fc4a79b2dbad72de
Nemya <yiduolnemya@gmail.com>
2025-01-22 21:48:25 -0500

    feat: create novel table schema

--- db/dbInit.js
+++ db/dbInit.js
@@ -3,3 +3,12 @@ const sequelize = new Sequelize({
   dialect: "sqlite",
   storage: "./database.sqlite",
 });
+const Novel = sequelize.define(
+  "Novel",
+  {
+    title: { type: DataTypes.TEXT, allowNull: false },
+    genre: DataTypes.TEXT,
+    language: { type: DataTypes.TEXT, allowNull: false },
+  },
+  { timestamps: true }
+);
