751213cc8391413e4242535fb91594ac236e785c
Nemya <yiduolnemya@gmail.com>
2025-01-29 21:20:00 -0500

    feat: kinda add entry dict to db

--- src/components/Chatbot.tsx
+++ src/components/Chatbot.tsx
@@ -4,13 +4,17 @@ import { useState } from "react";
 export default function Chatbot() {
   const [response, setResponse] = useState<string>("Hi! How can I assist you?");
   const [value, setValue] = useState<string>("");
+  const url = "http://localhost:8081";
   const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) =>
     setValue(e.target.value);
   const handleSubmit = async () => {
-    const response = await axios.post("http://localhost:8081/api/translate", {
+    const response = await axios.post(`${url}/api/translate`, {
       question: value,
     });
-    setResponse(response.data);
+    const text = response.data.translated_text;
+    const dict = response.data.dictionary;
+    await axios.post(`${url}/api/01`, { dict }); // TODO: Add novelID, find a way to fix this bs sourceTerm, targetTerm, sourceLanguage, targetLanguage
+    setResponse(text);
   };
   return (
     <div className="w-full">
