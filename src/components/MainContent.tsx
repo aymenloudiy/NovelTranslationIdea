2d89714a9358638f370635937031eba6f43d0844
Nemya <yiduolnemya@gmail.com>
2025-02-08 20:35:48 -0500

    break!: redo the ui from the scratch
    I was trying some new things and did not like it
    I am making it simple this next try

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,13 +1,5 @@
-import Chatbot from "./Chatbot";
 export default function MainContent() {
   return (
-    <main className="col-span-10 row-span-11 p-4 flex justify-stretch">
-      <Chatbot></Chatbot>
-
-      {/* <div>
-          <button>Raw</button>
-          <button>Translated</button>
-        </div> */}
-    </main>
+    <main className="col-span-10 row-span-11 p-4 flex justify-stretch"></main>
   );
 }
