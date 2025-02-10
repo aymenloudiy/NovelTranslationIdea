21c79e3b66a08dcda75e512d68c9249b8bca8640
Nemya <yiduolnemya@gmail.com>
2025-02-09 20:08:49 -0500

    feat: remove extra components
    feat: change to simpler navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  <>
+    <nav>
+      <ul>
+        <li>Translate</li>
+        <li>Library</li>
+        <li>Dictionary</li>
+      </ul>
+    </nav>
+  </>;
+}
