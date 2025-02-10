64275a8ffac1074265009926a2ca6d978c8703e4
Nemya <yiduolnemya@gmail.com>
2025-02-09 20:15:21 -0500

    feat: add mock data for novels and render a list

--- src/components/Library.tsx
+++ src/components/Library.tsx
@@ -1,3 +1,18 @@
 export default function Library() {
-  return <></>;
+  const _novels = [{ id: 0, title: "title1", language: "language1" }];
+  return (
+    <div>
+      <ul>
+        {_novels.map(({ id, title, language }) => {
+          return (
+            <li>
+              <div>{id}</div>
+              <div>{title}</div>
+              <div>{language}</div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
