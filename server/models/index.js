22a3c161115db3504f4d332d43cff2abf5ac5f0c
Nemya <yiduolnemya@gmail.com>
2025-03-01 13:25:32 -0500

    fix: disable logging

--- server/models/index.js
+++ server/models/index.js
@@ -2,6 +2,7 @@ import { Sequelize, DataTypes } from "sequelize";
 const sequelize = new Sequelize({
   dialect: "sqlite",
   storage: "./database.sqlite",
+  logging: "false",
 });
 
 const Novel = sequelize.define("Novel", {
