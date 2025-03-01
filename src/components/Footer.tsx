27db33f4b17b703226007ab485b8fa6b7f8a2e7e
Nemya <yiduolnemya@gmail.com>
2025-03-01 13:40:30 -0500

    feat: add footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,25 @@
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <div>
+        <a href="#">
+          <img src="" alt="Novel translation idea logo" />
+        </a>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">Resources</a>
+            </li>
+            <li>
+              <a href="#">FAQ</a>
+            </li>
+            <li>
+              <a href="#">Sign Up</a>
+            </li>
+          </ul>
+        </nav>
+        <small>&copy; 2025 Novel Translation Idea</small>
+      </div>
+    </footer>
+  );
 }
