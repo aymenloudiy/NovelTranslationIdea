dc8ac792fdb2002b21d560e2153419c6ea281727
Nimuel <yiduolnemya@gmail.com>
2025-04-23 20:46:45 -0400

    feat: add react router routes and error boundary

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { ErrorBoundary } from "react-error-boundary";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Terms from "./pages/TermsPage.tsx";
+import Error from "./pages/ErrorPage.tsx";
+import Home from "./pages/HomePage.tsx";
+import Library from "./pages/LibraryPage.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ErrorBoundary fallback={<Error />}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route path="home" element={<Home />} />
+            <Route path="library" element={<Library />} />
+            <Route path="terms" element={<Terms />} />
+            <Route path="*" element={<Error />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ErrorBoundary>
+  </StrictMode>
+);
